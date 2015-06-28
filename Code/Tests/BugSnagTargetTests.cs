2fe7816f672205b634ca9a4c07463ed5ff4dd1fe
Pure Krome <github@world-domination.com.au>
2015-06-28 17:49:04 +1000

    Trying to fix an AV failing test.

--- Code/Tests/BugSnagTargetTests.cs
+++ Code/Tests/BugSnagTargetTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,6 +56,8 @@ namespace Tests
             }
 
             // Assert.
+            Debug.WriteLine(error.ToString());
+            Console.WriteLine(error.ToString());
             error["events"][0]["exceptions"][0]["message"].ToString().ShouldBe(errorMessage);
             error["events"][0]["exceptions"][0]["stacktrace"].HasValues.ShouldBe(true);
             error["events"][0]["metaData"].Count().ShouldBe(1);
